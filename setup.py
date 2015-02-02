62bf614a78cc1eb6f9ffc9d97bdbfbee750cf983
six <brbsix@gmail.com>
2015-02-01 22:42:25 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from batchpath import __module__
 from batchpath import __version__
 
 
 def read(filename):
-    with open(filename) as fd:
-        return fd.read()
+    with open(filename) as f:
+        return f.read()
 
 
 setup(
-    name='batchpath',
+    name=__module__,
     version=__version__,
     author='Brian Beffa',
     author_email='brbsix@gmail.com',
@@ -18,7 +19,7 @@ setup(
     long_description=read('README.rst'),
     url='https://github.com/brbsix/python-batchpath',
     license='GPLv3',
-    keywords='os.walk, pathnames, paths',
+    keywords=['os.walk', 'pathnames', 'paths'],
     py_modules=['batchpath'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -26,6 +27,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
